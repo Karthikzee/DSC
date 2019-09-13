dff542bb99bfbfa91d4d12d097223e1a514b813d
Karthik <46178762+Karthikzee@users.noreply.github.com>
2019-09-13 11:41:07 +0530

    Add files via upload

---
+++ postfix_exp_eval_stack.c
@@ -0,0 +1,55 @@
+//Postfix Expression Evaluation
+#define SIZE 100
+#include <ctype.h>
+#include <stdio.h>
+int s[SIZE];
+int top =-1;
+void push(int elem)
+{
+	s[++top] = elem;
+}
+
+int pop()
+{
+	return(s[top--]);
+}
+int main(void)
+{
+	char pofx[50], ch;
+	int i = 0, op1, op2;
+	printf("Enter the Postfix Expression: \n");
+	scanf("%s", pofx);
+	while((ch = pofx[i++]) != '\0')
+	{
+		if(isdigit(ch))
+		{
+			push(ch - '0');
+		}
+		else
+		{
+			op2 = pop();
+			op1 = pop();
+			switch(ch)
+			{
+				case '+':
+					push(op1 + op2);
+					break;
+
+				case '-':
+					push(op1 - op2);
+					break;
+
+				case '*':
+					push(op1 * op2);
+					break;
+
+				case '/':
+					push(op1 / op2);
+					break;
+			}
+		}
+	}
+	printf("Given Postfix Expression: %s", pofx);
+	printf("\nResult after Evaluation: %d", s[top]);
+	printf("\n");
+}
